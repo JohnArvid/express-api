426562f4b6eafb4289162ed57bd9057123421a5e
JohnArvid <arvid.lindstrom@live.se>
2024-09-13 16:21:59 +0200

    add imports and routes defs to auth.js

--- auth.js
+++ auth.js
@@ -4,10 +4,31 @@
  * Required External Modules
  */
 
+import {Router} from "express"
+const router = Router()
+import passport from "passport"
+import queryString from "querystring"
+import dotenv from "dotenv"
+
+dotenv.config()
+
 /**
  * Routes Definitions
  */
+router.get(
+  "/login",
+  passport.authenticate("auth0", {
+    scope: "openid email profile"
+  }),
+  (req, res) => {
+    res.redirect("/")
+  }
 
+)
+
+router.get("/callback", (req, res, next) => {
+  
+})
 
 /**
  * Module Exports
